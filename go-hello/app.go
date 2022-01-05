0e1e7003b556bab6181b0d096fefb713966cd5b3
Naomi Lin <supakorn.t@ibm.com>
2022-01-05 15:01:36 +0700

    ADD

--- go-hello/app.go
+++ go-hello/app.go
@@ -21,7 +21,7 @@ func rootHandler(response http.ResponseWriter, request *http.Request) {
 
 	switch *lang {
 	case "en":
-		fmt.Fprintf(response, "Hello %s!. Bienvenido!\n", request.URL.Path[1:])
+		fmt.Fprintf(response, "Hola %s!. Bienvenido!\n", request.URL.Path[1:])
 	case "es":
 		fmt.Fprintf(response, "Hola %s!. Bienvenido!\n", request.URL.Path[1:])
 	default:
